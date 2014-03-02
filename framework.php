a4e7fd19fbd86a2fd1ec321df7879c7949b2e919
Aristeides Stathopoulos <aristath@gmail.com>
2014-03-02 10:41:54 +0200

    Aded github updater

--- framework.php
+++ framework.php
@@ -6,11 +6,16 @@ Description: Foundation support for the Shoestrap theme
 Version: 0.1
 Author: Aristeides Stathopoulos
 Author URI: http://aristeides.com
+GitHub Plugin URI: https://github.com/shoestrap/shoestrap-foundation
 */
 
 define( 'SS_FRAMEWORK', 'foundation' );
 define( 'SS_FRAMEWORK_PATH', dirname( __FILE__ ) );
 
+/**
+ * Add the framework class.
+ * We add this at using an action because it extends a class that has not been loaded yet.
+ */
 function ss_foundation() {
 	// Framework class.
 	require_once( dirname( __FILE__ ) . '/class-SS_Framework_Foundation.php' );
@@ -34,7 +39,8 @@ function ss_define_foundation() {
 add_filter( 'shoestrap_frameworks_array', 'ss_add_framework_foundation', 20 );
 
 /**
- * Add the framework to redux
+ * Add the framework to redux.
+ * This simply loads the array of frameworks that already exist, and adds a row to that array.
  */
 function ss_add_framework_foundation( $frameworks ) {
 	$frameworks[] = ss_define_foundation();
